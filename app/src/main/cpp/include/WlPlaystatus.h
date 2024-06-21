2fc97d668b23f1d8ceace14e96ca816b5054cdc0
胡国峥 <2554268076@xiaomi.com>
2024-06-21 22:09:39 +0800

    impl playstatus and queue

--- app/src/main/cpp/include/WlPlaystatus.h
+++ app/src/main/cpp/include/WlPlaystatus.h
@@ -1,4 +1,14 @@
 #ifndef AUDIOPLAYER_WLPLAYSTATUS_H
 #define AUDIOPLAYER_WLPLAYSTATUS_H
 
+class WlPlaystatus {
+public:
+    bool exit = false;
+    bool load = true;
+
+public:
+    WlPlaystatus();
+    ~WlPlaystatus();
+};
+
 #endif //AUDIOPLAYER_WLPLAYSTATUS_H
--- app/src/main/cpp/include/WlQueue.h
+++ app/src/main/cpp/include/WlQueue.h
@@ -1,4 +1,31 @@
 #ifndef AUDIOPLAYER_WLQUEUE_H
 #define AUDIOPLAYER_WLQUEUE_H
 
+#include "queue"
+#include "pthread.h"
+#include "WlPlaystatus.h"
+#include <android/log.h>
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+};
+
+class WlQueue {
+public:
+    std::queue<AVPacket *> queuePacket;
+    pthread_mutex_t mutexPacket;
+    pthread_cond_t condPacket;
+    WlPlaystatus *playstatus = NULL;
+
+public:
+    WlQueue(WlPlaystatus *playstatus);
+    ~WlQueue();
+
+    int putAvpacket(AVPacket *packet);
+    int getAvpacket(AVPacket *packet);
+
+    int getQueueSize();
+    void clearAvpacket();
+};
+
 #endif //AUDIOPLAYER_WLQUEUE_H
