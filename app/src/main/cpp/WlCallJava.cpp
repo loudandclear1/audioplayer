33b7f7da90bc0c8be09d3c62a8d502d73be9f5c1
胡国峥 <2554268076@xiaomi.com>
2024-06-21 22:34:56 +0800

    add wlCallJava

--- app/src/main/cpp/WlCallJava.cpp
+++ app/src/main/cpp/WlCallJava.cpp
@@ -1,1 +1,34 @@
 #include "WlCallJava.h"
+
+WlCallJava::WlCallJava(_JavaVM *javaVM, JNIEnv *env, jobject *obj) {
+    this->javaVM = javaVM;
+    this->jniEnv = env;
+    this->jobj = *obj;
+    this->jobj = env->NewGlobalRef(jobj);
+
+    jclass jlz = jniEnv->GetObjectClass(jobj);
+    if(!jlz) {
+        return;
+    }
+
+    jmid_prepared = env->GetMethodID(jlz, "onCallPrepared", "()V");
+    jmid_load = env->GetMethodID(jlz, "onCallLoad", "(Z)V");
+    jmid_timeinfo = env->GetMethodID(jlz, "onCallTimeInfo", "(II)V");
+
+}
+
+void WlCallJava::onCallPrepared(int type) {
+
+}
+
+void WlCallJava::onCallLoad(int type, bool load) {
+
+}
+
+void WlCallJava::onCallTimeInfo(int type, int curr, int total) {
+
+}
+
+WlCallJava::~WlCallJava() {
+    
+}
--- app/src/main/cpp/include/WlCallJava.h
+++ app/src/main/cpp/include/WlCallJava.h
@@ -1,4 +1,32 @@
 #ifndef AUDIOPLAYER_WLCALLJAVA_H
 #define AUDIOPLAYER_WLCALLJAVA_H
 
+#include "jni.h"
+#include <linux/stddef.h>
+#include "AndroidLog.h"
+
+#define MAIN_THREAD 0
+#define CHILD_THREAD 1
+
+class WlCallJava {
+public:
+    _JavaVM *javaVM = NULL;
+    JNIEnv *jniEnv = NULL;
+    jobject jobj;
+
+    jmethodID jmid_prepared;
+    jmethodID jmid_load;
+    jmethodID jmid_timeinfo;
+
+public:
+    WlCallJava(_JavaVM *javaVM, JNIEnv *env, jobject *obj);
+    ~WlCallJava();
+
+    void onCallPrepared(int type);
+
+    void onCallLoad(int type, bool load);
+
+    void onCallTimeInfo(int type, int curr, int total);
+};
+
 #endif //AUDIOPLAYER_WLCALLJAVA_H
