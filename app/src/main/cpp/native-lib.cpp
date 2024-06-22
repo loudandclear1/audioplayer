96cdbb28e195f3a619e1fdf29479e79f87e17197
胡国峥 <2554268076@xiaomi.com>
2024-06-22 15:21:53 +0800

    test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,29 +1,71 @@
 #include <jni.h>
 #include <string>
+#include "WlFFmpeg.h"
+#include "WlPlaystatus.h"
 
+extern "C" {
+#include "libavformat/avformat.h"
+}
+
+_JavaVM *javaVM = NULL;
+WlCallJava *callJava = NULL;
+WlFFmpeg *fFmpeg = NULL;
+WlPlaystatus *playstatus = NULL;
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_hgz_audioplayer_player_WlPlayer_n_1prepared(JNIEnv *env, jobject thiz, jstring source) {
-    // TODO: implement n_prepared()
+Java_com_hgz_audioplayer_player_WlPlayer_n_1prepared(JNIEnv *env, jobject thiz, jstring source_) {
+    const char *source = env->GetStringUTFChars(source_, 0);
+
+    if (fFmpeg == NULL) {
+        if (callJava == NULL) {
+            callJava = new WlCallJava(javaVM, env, &thiz);
+        }
+        callJava->onCallLoad(MAIN_THREAD, true);
+        playstatus = new WlPlaystatus();
+        fFmpeg = new WlFFmpeg(playstatus, callJava, source);
+        fFmpeg->prepared();
+    }
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_hgz_audioplayer_player_WlPlayer_n_1start(JNIEnv *env, jobject thiz) {
-    // TODO: implement n_start()
+    if(fFmpeg != NULL) {
+        fFmpeg->start();
+    }
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_hgz_audioplayer_player_WlPlayer_n_1pause(JNIEnv *env, jobject thiz) {
-    // TODO: implement n_pause()
+    if(fFmpeg != NULL) {
+        fFmpeg->pause();
+    }
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_hgz_audioplayer_player_WlPlayer_n_1resume(JNIEnv *env, jobject thiz) {
-    // TODO: implement n_resume()
+    if(fFmpeg != NULL) {
+        fFmpeg->resume();
+    }
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_hgz_audioplayer_player_WlPlayer_n_1stop(JNIEnv *env, jobject thiz) {
-    // TODO: implement n_stop()
+    if(fFmpeg != NULL) {
+        fFmpeg->release();
+        delete(fFmpeg);
+        fFmpeg = NULL;
+        if(callJava != NULL) {
+            delete(callJava);
+            callJava = NULL;
+        }
+        if(playstatus != NULL) {
+            delete(playstatus);
+            playstatus = NULL;
+        }
+    }
 }
